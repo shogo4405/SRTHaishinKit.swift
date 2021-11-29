852da5c735615672825a871dc27342770fc288fe
Allan Bush <allan@oooh.tv>
2021-11-29 10:28:50 -0800

    Add option to be the SRT listener
    Made the incoming soxket in the caller mode optional since it's not required for the connection to work
    Throwing errors in the connect call if somethings goes wrong
    Added a dynamic `connectionBroken` property that can be observed to detect when the network connection is dropped

--- Sources/SRT/SRTIncomingSocket.swift
+++ Sources/SRT/SRTIncomingSocket.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 final class SRTIncomingSocket: SRTSocket {
-    override func configure(_ binding: SRTSocketOption.Binding) -> Bool {
+    override func configure(_ binding: SRTSocketOption.Binding, _ sock: SRTSOCKET) -> Bool {
         switch binding {
         case .pre:
-            return super.configure(binding)
+            return super.configure(binding, sock)
         case .post:
             options[.rcvsyn] = true
             if 0 < timeout {
                 options[.rcvtimeo] = timeout
             }
-            return super.configure(binding)
+            return super.configure(binding, sock)
         }
     }
 }
--- Sources/SRT/SRTSocket.swift
+++ Sources/SRT/SRTSocket.swift
@@ -21,6 +21,8 @@ class SRTSocket {
 
     private let lockQueue: DispatchQueue = DispatchQueue(label: "com.haishinkit.SRTHaishinKit.SRTSocket.lock")
     private(set) var socket: SRTSOCKET = SRT_INVALID_SOCK
+    private(set) var bindSocket: SRTSOCKET = SRT_INVALID_SOCK
+    
     private(set) var status: SRT_SOCKSTATUS = SRTS_INIT {
         didSet {
             guard status != oldValue else { return }
@@ -66,17 +68,14 @@ class SRTSocket {
         }
 
         // prepare socket
-        socket = srt_socket(AF_INET, SOCK_DGRAM, 0)
-        if socket == SRT_ERROR {
-                let error_message = String(cString: srt_getlasterror_str())
-
-                logger.error(error_message)
-                throw SRTError.illegalState(message: error_message)
+        socket = srt_create_socket()
+        if socket == SRT_INVALID_SOCK {
+            throw createConnectionException()
         }
 
         self.options = options
-        guard configure(.pre) else {
-            return
+        guard configure(.pre, socket) else {
+            throw createConnectionException()
         }
 
         // prepare connect
@@ -87,32 +86,150 @@ class SRTSocket {
         }
 
         if stat == SRT_ERROR {
-
-            let error_message = String(cString: srt_getlasterror_str())
-
-            logger.error(error_message)
-            throw SRTError.illegalState(message: error_message)
+            throw createConnectionException()
         }
-
-        guard configure(.post) else {
-            return
+        
+        guard configure(.post, socket) else {
+            throw createConnectionException()
         }
 
         startRunning()
     }
+    
+    private func createConnectionException() -> SRTError {
+        let error_message = String(cString: srt_getlasterror_str())
 
-    func close() {
-        guard socket != SRT_INVALID_SOCK else { return }
-        srt_close(socket)
-        socket = SRT_INVALID_SOCK
+        logger.error(error_message)
+        return SRTError.illegalState(message: error_message)
     }
 
-    func configure(_ binding: SRTSocketOption.Binding) -> Bool {
-        let failures = SRTSocketOption.configure(socket, binding: binding, options: options)
+    func close() {
+        if(socket != SRT_INVALID_SOCK) {
+            srt_close(socket)
+            socket = SRT_INVALID_SOCK
+        }
+        if bindSocket != SRT_INVALID_SOCK {
+            srt_close(bindSocket)
+            bindSocket = SRT_INVALID_SOCK
+        }
+    }
+    
+    func configure(_ binding: SRTSocketOption.Binding, _ sock: SRTSOCKET) -> Bool {
+        let failures = SRTSocketOption.configure(sock, binding: binding, options: options)
         guard failures.isEmpty else {
             logger.error(failures); return false
         }
         return true
+    }
+    
+    func listen(_ addr: sockaddr_in, options: [SRTSocketOption: Any] = SRTSocket.defaultOptions) throws {
+        guard bindSocket == SRT_INVALID_SOCK else { return }
+        
+        self.options = options
+        
+        // Create a socket, bind it and start listening for connections
+        bindSocket = srt_create_socket()
+        if bindSocket == SRT_INVALID_SOCK {
+            throw createConnectionException()
+        }
+
+        guard configure(.pre, bindSocket) else { return }
+        
+        var addr_cp = addr
+        var stat = withUnsafePointer(to: &addr_cp) { ptr -> Int32 in
+            let psa = UnsafeRawPointer(ptr).assumingMemoryBound(to: sockaddr.self)
+            return srt_bind(bindSocket, psa, Int32(MemoryLayout.size(ofValue: addr)))
+        }
+        if stat == SRT_ERROR {
+            throw createConnectionException()
+        }
+        // only supporting a single connection
+        stat = srt_listen(bindSocket, 1)
+        if stat == SRT_ERROR {
+            srt_close(bindSocket)
+            throw createConnectionException()
+        }
+        
+        // setup polling of the socket to manage incoming connections
+        let eid = srt_epoll_create()
+        guard eid >= 0 else {
+            srt_close(bindSocket)
+            throw createConnectionException()
+        }
+        var eventMask:Int32 = Int32(SRT_EPOLL_IN.rawValue)
+        stat = srt_epoll_add_usock(eid, bindSocket, &eventMask)
+        if stat == SRT_ERROR {
+            srt_close(bindSocket)
+            throw createConnectionException()
+        }
+        
+        // ensure everything is setup correctly and then move listening to another thread
+        self.status = srt_getsockstate(bindSocket)
+        if(self.status != SRTS_LISTENING) {
+            srt_close(bindSocket)
+            throw createConnectionException()
+        }
+        
+        DispatchQueue(label:"com.HaishkinKit.SRTSocket.listen").async {
+            do {
+             try self.waitForConnection(eid)
+            } catch {
+                logger.error("Issue while processing data on listener socket: ", error)
+                self.status = SRTS_BROKEN
+            }
+        }
+    }
+    
+    private func waitForConnection(_ eid: Int32) throws {
+               
+        // only deal 1 event
+        let events: UnsafeMutablePointer<SRT_EPOLL_EVENT> = UnsafeMutablePointer<SRT_EPOLL_EVENT>.allocate(capacity: 1)
+        while true {
+            
+            if bindSocket == SRT_INVALID_SOCK {
+                logger.info("Stopped listening")
+                return
+            }
+            
+            // poll for an incoming connection
+            let eventCount = srt_epoll_uwait(eid, events, 1, 100)
+            if eventCount > 0 {
+                let event = events.pointee
+                logger.trace("Listening event: ", event)
+                try self.completeConnection()
+                srt_epoll_remove_usock(eid, event.fd)
+                return
+            }
+        }
+    }
+    
+    private func completeConnection() throws {
+        let status = srt_getsockstate(bindSocket)
+        
+        guard status == SRTS_LISTENING else {
+            return
+        }
+        
+        // the caller has connected to our listening socket accept the connection
+        socket = srt_accept(bindSocket, nil, nil)
+        
+        // only working with one connection can stop listening for more now
+        srt_close(bindSocket)
+        bindSocket = SRT_INVALID_SOCK
+        
+        if socket == SRT_INVALID_SOCK {
+            throw createConnectionException()
+        }
+        
+        guard configure(.post, socket) else {
+            throw createConnectionException()
+        }
+        
+        if srt_getsockstate(socket) != SRTS_CONNECTED {
+            throw createConnectionException()
+        }
+        
+        startRunning()
     }
 }
 
