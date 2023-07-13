7a43413e20422f6c94e68680c66e37122a45f952
shogo4405 <shogo4405@gmail.com>
2023-07-14 00:56:03 +0900

    fixed #61

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "SRTHaishinKit", targets: ["SRTHaishinKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/shogo4405/HaishinKit.swift.git", branch: "1.5.3")
+        .package(url: "https://github.com/shogo4405/HaishinKit.swift.git", from: "1.5.3")
     ],
     targets: [
         .binaryTarget(
